f89e6748d8a6e795a880903f6e3cf1b49b2e09b5
Yaidekar Sudeep <yaidekarsudeep897@gmail.com>
2025-10-15 22:08:03 +0530

    Add files via upload

---
+++ Hw1.py
@@ -0,0 +1,23 @@
+
+#arthimatic opertions
+a=18
+b=12
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+print(a+b-a%b-a/b**a)
+
+
+#swap two numbers
+
+s=17
+v=24
+print(s,v)
+s,v=v,s
+print(s,v)
