9f1b8a09e25d27de1163a8dd46ae02167abbf4cf
Yaidekar Sudeep <yaidekarsudeep897@gmail.com>
2025-10-16 09:32:09 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,43 @@
+# Operation in Python
+
+# assignment opertor
+
+v=4
+w=5
+x=8
+y=6
+z=35
+
+v+=5 # v=v+5
+w-=3 # w=v-3
+x*=5 # x=x*5
+y/=2 # y=y/2
+z%=5 # z=z%5
+print(v)
+print(w)
+print(x)
+print(y)
+print(z)
+
+#comperison operators
+s=3857
+v=2474
+
+print(s == v)
+print(s != v)
+print(s > v)
+print(s < v)
+print(s >= v)
+print(s <= v)
+
+#logical operators
+
+mat=22
+ddco=24
+os=25
+opp=18
+dsa=15
+
+print(mat<os and dsa<ddco)
+print(opp>dsa or opp>ddco)
+print(not(dsa<mat))
